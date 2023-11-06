9797fe3d8d7a2fee6ca23fe61bd051208fe484c8
prabhakaran <prabhakaranplkk@gmail.com>
2023-11-06 14:48:07 +0530

    bind

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let runner = {
+    name: 'Runner',
+    run: function(speed) {
+        console.log(this.name + ' runs at ' + speed + ' mph.');
+    }
+};
+
+let flyer = {
+    name: 'Flyer',
+    fly: function(speed) {
+        console.log(this.name + ' flies at ' + speed + ' mph.');
+    }
+};
+
+let res=runner.run.bind(flyer,20);
+res()
